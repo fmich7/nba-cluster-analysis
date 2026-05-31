d9dadcf9a208b9e4f25397ed2222fd6d944ca7e5
MarekNajmola <marek.najmola2005@gmail.com>
2026-05-31 17:16:47 +0200

    visual representation
    optimized algorithm and added grantt chart representation

--- grafy/generator_danych.py
+++ grafy/generator_danych.py
@@ -29,8 +29,12 @@ def generuj_dane_konferencji(liczba_sesji, szansa_na_konflikt,mindlugosc,maxdlug
         if sala["rozmiar"] > max_rozmiar_sali:
             max_rozmiar_sali = sala["rozmiar"]
         sale.append(sala) 
-        if max_rozmiar_sali < max_rozmiar_sesji:
-            sesje[0]["rozmiar"] = max_rozmiar_sali
+
+    if max_rozmiar_sali < max_rozmiar_sesji:
+        print(f"zmieniam rozmiar sali z {max_rozmiar_sali} na {max_rozmiar_sesji}")
+        sale[0]["rozmiar"] = max_rozmiar_sesji
+        
+
     for i in range(liczba_sesji):
         for j in range(i + 1, liczba_sesji):
             if random.random() < szansa_na_konflikt:
@@ -41,14 +45,14 @@ def generuj_dane_konferencji(liczba_sesji, szansa_na_konflikt,mindlugosc,maxdlug
     return sesje, konflikty,sale
 
 
-sesje, konflikty,sale = generuj_dane_konferencji(liczba_sesji=8, szansa_na_konflikt=0.3, mindlugosc=15, maxdlugosc=120, ilosc_sali=3)
+# sesje, konflikty,sale = generuj_dane_konferencji(liczba_sesji=8, szansa_na_konflikt=0.3, mindlugosc=15, maxdlugosc=120, ilosc_sali=3)
 
 # Wypisanie wyniku w postaci zwykłego tekstu
 # print("--- DANE WEJŚCIOWE KONFERENCJI ---")
 # print(f"Liczba sesji: {len(sesje)}")
 # print(f"Lista sesji: {', '.join(x['nazwa'] for x in sesje)}")
 # print(f"czas trwania sesji: {', '.join(str(x['dlugosc']) for x in sesje)}")
-print(f"Rozmiar sesji: {', '.join(str(x['rozmiar']) for x in sesje)}")
-print(f"rozmiary sali: {', '.join(str(x['rozmiar']) for x in sale)}")
+# print(f"Rozmiar sesji: {', '.join(str(x['rozmiar']) for x in sesje)}")
+# print(f"rozmiary sali: {', '.join(str(x['rozmiar']) for x in sale)}")
 # print(f"Liczba konfliktów: {len(konflikty)}")
 # print(konflikty[0][0])
--- grafy/planowanie_konferencji.py
+++ grafy/planowanie_konferencji.py
@@ -1,29 +1,59 @@
 import generator_danych
-sesje, konflikty,sale = generator_danych.generuj_dane_konferencji(liczba_sesji=20, szansa_na_konflikt=0.3, mindlugosc=60, maxdlugosc=60, ilosc_sali=3)
-sesje.sort(reverse=True,key=lambda x: (x['ilosc_konfliktow'],x['dlugosc'], x['rozmiar']))
-sale.sort(reverse=True,key=lambda x: x['rozmiar'])
-for i in range(len(sesje)):
-    wpisane = False
-    for j in range(len(sale)):
-        print(sesje[i]["nazwa"] ,sale[j]["nazwa"])
-        if sale[j]["rozmiar"]>=sesje[i]["rozmiar"]:
-            for k in range(32):
-                if sale[j]["harmonogram"][k] is None:
-                    if k+sesje[i]["dlugosc"]//15<=32:
-                        for l1 in range(k,k+sesje[i]["dlugosc"]//15):
-                            for konflikt in konflikty:
-                                if konflikt[0]["nazwa"]==sesje[i]["nazwa"] and konflikt[1]["nazwa"]==sale[j]["harmonogram"][l1]:
-                                    break
-                        for l2 in range(k,k+sesje[i]["dlugosc"]//15):
-                            sale[j]["harmonogram"][l2] = sesje[i]["nazwa"]
-                        wpisane = True
-                        break
-        if wpisane==True:
-            break
-    if wpisane==False:
-        print("nie można wpisać sesji ",sesje[i]["nazwa"],sesje[i]["ilosc_konfliktow"]," do żadnej sali")    
-print(sale[0]["harmonogram"])
-print("\n")
-print(sale[1]["harmonogram"])
-print("\n")
-print(sale[2]["harmonogram"])
+import plotly.express as px
+import pandas as pd
+import datetime
+def harmonogram(sesje, konflikty, sale,przerwy):
+    sesje.sort(reverse=True,key=lambda x: (x['rozmiar'],x['ilosc_konfliktow'], x['dlugosc']))
+    sale.sort(reverse=True,key=lambda x: x['rozmiar'])
+    sesje_wykres=[]
+    for i in range(len(sesje)):
+        print(sesje[i]["nazwa"],sesje[i]["ilosc_konfliktow"],sesje[i]["rozmiar"])
+        wpisane = False
+        for j in range(len(sale)):
+            if sale[j]["rozmiar"]>=sesje[i]["rozmiar"]:
+                for k in range(32):
+                    if sale[j]["harmonogram"][k] is None:
+                        if k+sesje[i]["dlugosc"]//15<=32:
+                            for l1 in range(k,k+sesje[i]["dlugosc"]//15):
+                                for konflikt in konflikty:
+                                    if konflikt[0]["nazwa"]==sesje[i]["nazwa"] and konflikt[1]["nazwa"]==sale[j]["harmonogram"][l1]:
+                                        break
+                            for l2 in range(k,k+sesje[i]["dlugosc"]//15):
+                                sale[j]["harmonogram"][l2] = sesje[i]["nazwa"]
+                            for l3 in range(k+sesje[i]["dlugosc"]//15,k+(sesje[i]["dlugosc"]+przerwy)//15):
+                                    if l3<32:
+                                        sale[j]["harmonogram"][l3] = "przerwa"
+                            sesje_wykres.append(dict(Nazwa=sesje[i]["nazwa"],
+                                                    Sala=sale[j]["nazwa"], 
+                                                    Start= datetime.datetime(2025, 5, 31, 9, 0)+ datetime.timedelta(minutes=k*15), 
+                                                    Koniec= datetime.datetime(2025, 5, 31, 9, 0)+ datetime.timedelta(minutes=(k+sesje[i]["dlugosc"]//15)*15)))
+                            wpisane = True
+                            break
+            if wpisane==True:
+                break
+        if wpisane==False:
+            print("nie można wpisać sesji ",sesje[i]["nazwa"],sesje[i]["ilosc_konfliktow"]," do żadnej sali")    
+    df = pd.DataFrame(sesje_wykres)
+    wykres=px.timeline(
+        df,
+        x_start="Start",
+        x_end="Koniec",
+        y="Sala",
+        color="Nazwa",
+        text="Nazwa",
+        title="Harmonogram konferencji"
+    )
+    wykres.update_yaxes(autorange="reversed")
+    wykres.update_xaxes(
+        tickformat="%H:%M",
+        dtick=1800000
+    )
+    wykres.update_traces(textposition='inside', insidetextanchor='middle')
+    wykres.show()
+    print(sale[0]["harmonogram"],sale[0]["rozmiar"])
+    print("\n")
+    print(sale[1]["harmonogram"],sale[1]["rozmiar"])
+    print("\n")
+    print(sale[2]["harmonogram"],sale[2]["rozmiar"])
+sesje, konflikty,sale = generator_danych.generuj_dane_konferencji(liczba_sesji=20, szansa_na_konflikt=0.2, mindlugosc=30, maxdlugosc=90, ilosc_sali=3)
+harmonogram(sesje, konflikty, sale, przerwy=15)
