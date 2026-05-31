4046e81d8709c5b068671bd7df6eeaf62871f694
MarekNajmola <marek.najmola2005@gmail.com>
2026-05-31 03:26:57 +0200

    algorithm

--- grafy/generator_danych.py
+++ grafy/generator_danych.py
@@ -8,13 +8,8 @@ def generuj_dane_konferencji(liczba_sesji, szansa_na_konflikt,mindlugosc,maxdlug
     sesje = []
     konflikty = []
     sale=[]
-    for i in range(ilosc_sali):
-        sala={
-            "nazwa": f"Sala{i+1}",
-            "rozmiar": random.randint(1, 3) * 50,
-            "harmonogram": [None] * 32
-        }
-        sale.append(sala)
+    max_rozmiar_sesji=0
+    max_rozmiar_sali=0
     for i in range(liczba_sesji):
         sesja = {
             "nazwa": f"S{i+1}",
@@ -22,7 +17,20 @@ def generuj_dane_konferencji(liczba_sesji, szansa_na_konflikt,mindlugosc,maxdlug
             "rozmiar": random.randint(1, 3) * 50,
             "ilosc_konfliktow": 0
         }
+        if sesja["rozmiar"]>max_rozmiar_sesji:
+            max_rozmiar_sesji = sesja["rozmiar"]
         sesje.append(sesja)
+    for i in range(ilosc_sali):
+        sala={
+            "nazwa": f"Sala{i+1}",
+            "rozmiar": random.randint(1, 3) * 50,
+            "harmonogram": [None] * 32
+        }
+        if sala["rozmiar"] > max_rozmiar_sali:
+            max_rozmiar_sali = sala["rozmiar"]
+        sale.append(sala) 
+        if max_rozmiar_sali < max_rozmiar_sesji:
+            sesje[0]["rozmiar"] = max_rozmiar_sali
     for i in range(liczba_sesji):
         for j in range(i + 1, liczba_sesji):
             if random.random() < szansa_na_konflikt:
@@ -36,10 +44,11 @@ def generuj_dane_konferencji(liczba_sesji, szansa_na_konflikt,mindlugosc,maxdlug
 sesje, konflikty,sale = generuj_dane_konferencji(liczba_sesji=8, szansa_na_konflikt=0.3, mindlugosc=15, maxdlugosc=120, ilosc_sali=3)
 
 # Wypisanie wyniku w postaci zwykłego tekstu
-print("--- DANE WEJŚCIOWE KONFERENCJI ---")
-print(f"Liczba sesji: {len(sesje)}")
-print(f"Lista sesji: {', '.join(x['nazwa'] for x in sesje)}")
-print(f"czas trwania sesji: {', '.join(str(x['dlugosc']) for x in sesje)}")
-print(f"Rozmiar sali: {', '.join(str(x['rozmiar']) for x in sesje)}")
+# print("--- DANE WEJŚCIOWE KONFERENCJI ---")
+# print(f"Liczba sesji: {len(sesje)}")
+# print(f"Lista sesji: {', '.join(x['nazwa'] for x in sesje)}")
+# print(f"czas trwania sesji: {', '.join(str(x['dlugosc']) for x in sesje)}")
+print(f"Rozmiar sesji: {', '.join(str(x['rozmiar']) for x in sesje)}")
 print(f"rozmiary sali: {', '.join(str(x['rozmiar']) for x in sale)}")
-print(f"Liczba konfliktów: {len(konflikty)}")
+# print(f"Liczba konfliktów: {len(konflikty)}")
+# print(konflikty[0][0])
--- grafy/planowanie_konferencji.py
+++ grafy/planowanie_konferencji.py
@@ -1,6 +1,29 @@
 import generator_danych
-sesje, konflikty,sale = generator_danych.generuj_dane_konferencji(liczba_sesji=8, szansa_na_konflikt=0.3, mindlugosc=15, maxdlugosc=120, ilosc_sali=3)
+sesje, konflikty,sale = generator_danych.generuj_dane_konferencji(liczba_sesji=20, szansa_na_konflikt=0.3, mindlugosc=60, maxdlugosc=60, ilosc_sali=3)
 sesje.sort(reverse=True,key=lambda x: (x['ilosc_konfliktow'],x['dlugosc'], x['rozmiar']))
 sale.sort(reverse=True,key=lambda x: x['rozmiar'])
-print (sesje)
-
+for i in range(len(sesje)):
+    wpisane = False
+    for j in range(len(sale)):
+        print(sesje[i]["nazwa"] ,sale[j]["nazwa"])
+        if sale[j]["rozmiar"]>=sesje[i]["rozmiar"]:
+            for k in range(32):
+                if sale[j]["harmonogram"][k] is None:
+                    if k+sesje[i]["dlugosc"]//15<=32:
+                        for l1 in range(k,k+sesje[i]["dlugosc"]//15):
+                            for konflikt in konflikty:
+                                if konflikt[0]["nazwa"]==sesje[i]["nazwa"] and konflikt[1]["nazwa"]==sale[j]["harmonogram"][l1]:
+                                    break
+                        for l2 in range(k,k+sesje[i]["dlugosc"]//15):
+                            sale[j]["harmonogram"][l2] = sesje[i]["nazwa"]
+                        wpisane = True
+                        break
+        if wpisane==True:
+            break
+    if wpisane==False:
+        print("nie można wpisać sesji ",sesje[i]["nazwa"],sesje[i]["ilosc_konfliktow"]," do żadnej sali")    
+print(sale[0]["harmonogram"])
+print("\n")
+print(sale[1]["harmonogram"])
+print("\n")
+print(sale[2]["harmonogram"])
